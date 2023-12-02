62716c8e203efd880e2b34d7ca7a8bfc7d0a3e2c
Yash Raj <yash_2001ee83@iitp.ac.in>
2023-12-03 02:59:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
+import { lazy, Suspense, useState } from "react";
+import Auth from "./pages/Auth";
+import ImageUpload from "./pages/ImageUpload";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ToastContainer autoClose={2000} />
+        <AppContent />
+      </BrowserRouter>
     </div>
+  );
+}
+
+function AppContent(props) {
+  const location = useLocation();
+
+  return (
+    <>
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/Img" element={<ImageUpload />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 }
 
---
+++ src/pages/ImageUpload.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import "./ImageUpload.css";
+
+const ImageUpload = () => {
+  const [mode, setMode] = useState("Digit"); // Default mode is Digit
+
+  const [prediction, setPrediction] = useState("");
+  const [base64Img, setBase64Img] = useState("");
+  const [upImg, setUpImg] = useState("");
+
+  const uploadImage = async () => {
+    const fileInput = document.getElementById("imageInput");
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+    setUpImg(fileInput.files[0]);
+    try {
+      const response = await fetch("http://127.0.0.1:8000/hindi", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // console.log(data);
+        setBase64Img(data.transformed_image);
+        setPrediction(data.predicted_label);
+        // Handle the data or update the state as needed
+      } else {
+        throw new Error("Upload failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      // Handle error or show a message to the user
+    }
+  };
+
+  const handleModeChange = (e) => {
+    setMode(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="heading">
+        <h1>Image classification portal</h1>
+      </div>
+      <div className="container">
+        <h1>Image Upload</h1>
+        <form id="uploadForm" encType="multipart/form-data">
+          <input type="file" name="file" id="imageInput" accept="image/*" />
+          <div className="radio-buttons">
+            <label>
+              <input
+                type="radio"
+                value="Digit"
+                checked={mode === "Digit"}
+                onChange={handleModeChange}
+              />
+              Digit
+            </label>
+            <label>
+              <input
+                type="radio"
+                value="Letter"
+                checked={mode === "Letter"}
+                onChange={handleModeChange}
+              />
+              Letter
+            </label>
+          </div>
+          <button type="button" onClick={uploadImage}>
+            Upload
+          </button>
+        </form>
+        <div id="message"></div>
+      </div>
+      <div className="text">
+        <h1>Text translated</h1>
+        <div className="container box">
+          {/* Content based on the current mode */}
+          {mode === "Digit" && <p>Digit mode content</p>}
+          {mode === "Letter" && base64Img && (
+            <div style={{ textAlign: "center" }}>
+              {/* Display predicted value */}
+              <p style={{ fontSize: "20px" }}>{prediction}</p>
+
+              {/* Display the images */}
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <div style={{ margin: "10px" }}>
+                  {/* Display the uploaded image */}
+                  {base64Img && (
+                    <div>
+                      <img
+                        src={URL.createObjectURL(upImg)}
+                        alt="Uploaded Image"
+                        style={{ width: "200px", height: "200px" }}
+                      />
+                      <p>Uploaded Image</p>
+                    </div>
+                  )}
+                </div>
+                {base64Img && (
+                <div style={{ margin: "10px" }}>
+                  {/* Display the transformed image */}
+                  <div>
+                    <img
+                      src={`data:image/png;base64,${base64Img}`}
+                      alt="Transformed Image"
+                      style={{ width: "200px", height: "200px" }}
+                    />
+                    <p>Transformed Image</p>
+                  </div>
+                </div>
+                )}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
